b924f300d1570a4a3d53707a9bee42185a943c01
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2019-12-01 00:03:02 +0900

    Update bode_fdi to plot Coherence

--- src/A_CalculationAuxiliary/bode_fdi.m
+++ src/A_CalculationAuxiliary/bode_fdi.m
@@ -1,8 +1,9 @@
 function hfig = bode_fdi(data,noise,option)
-% 
+% number of plot
+if isfield(data.UserData,'cxy'), Nplot = 3; else, Nplot = 2; end
 
 if nargin < 2
-    noise = 'sCR';
+    if isfield(data.UserData,'cxy'), noise = 'cxy'; else, noise = 'sCR'; end
 end
 
 if nargin < 3
@@ -21,13 +22,13 @@ for k = 1:N
 end
 
 hfig = figure;
-subplot(2,1,1);
+subplot(Nplot,1,1);
 for k = 1:N
     h = semilogx(data{k}.frequency,mag2db(abs(squeeze(data{k}.ResponseData)))); hold on;
 end
 ylabel('Magnitude [dB]');
 
-subplot(2,1,2);
+subplot(Nplot,1,2);
 for k = 1:N
     phasedeg = rad2deg(angle(squeeze(data{k}.ResponseData)));
     for kk = 1:length(phasedeg)
@@ -43,17 +44,26 @@ for k = 1:N
     ylim([option.pmin,option.pmax]);
 end
 ylabel('Phase [deg]');
-xlabel(['Frequency [',data{1}.FrequencyUnit,']']);
 
-subplot(2,1,1);
-if ischar(noise)
+if isfield(data{k}.UserData,'cxy')
+    subplot(Nplot,1,3);
     for k = 1:N
-        if isfield(data{k}.UserData,noise)
-            h = semilogx(data{k}.freq,mag2db(abs(getfield(data{k}.UserData,noise)))); hold on;
-        end
+        h = semilogx(data{k}.frequency,data{k}.UserData.cxy); hold on;
     end
+    ylabel('Coherence [-]');
 else
-    h = semilogx(noise(:,1),mag2db(abs(noise(:,2)))); hold on;
+    subplot(2,1,1);
+    if ischar(noise)
+        for k = 1:N
+            if isfield(data{k}.UserData,noise)
+                h = semilogx(data{k}.freq,mag2db(abs(getfield(data{k}.UserData,noise)))); hold on;
+            end
+        end
+    else
+        h = semilogx(noise(:,1),mag2db(abs(noise(:,2)))); hold on;
+    end
 end
+
+xlabel(['Frequency [',data{1}.FrequencyUnit,']']);
 
 end
