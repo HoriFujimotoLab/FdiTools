c6fcb84ea137329429337e179979f08b2db55a9b
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2019-12-11 14:57:44 +0900

    Update fdicohere.m

--- src/A_CalculationAuxiliary/fdicohere.m
+++ src/A_CalculationAuxiliary/fdicohere.m
@@ -3,10 +3,17 @@ function Pest = fdicohere(Pest)
 % Wataru Ohnishi, the University of Tokyo, 2019
 %%%%
 
-nrofs = length(Pest.UserData.ms.x);
+nrofs = Pest.UserData.ms.nrofs;
 fs = Pest.UserData.ms.harm.fs;
-[cxy,~] = mscohere(Pest.UserData.x,Pest.UserData.y,...
+[cxy,freq] = mscohere(Pest.UserData.x,Pest.UserData.y,...
     rectwin(nrofs),0,nrofs,fs);
-Pest.UserData.cxy = cxy(Pest.UserData.ms.ex,:);
+
+if isfield(Pest.UserData.ms,'ex')
+    Pest.UserData.cxy = cxy(Pest.UserData.ms.ex,:);
+else
+    [~,kmin] = min(abs(freq-Pest.UserData.ms.harm.fl));
+    [~,kmax] = min(abs(freq-Pest.UserData.ms.harm.fh));
+    Pest.UserData.cxy = cxy(kmin:kmax,:);
+end
 
 end
